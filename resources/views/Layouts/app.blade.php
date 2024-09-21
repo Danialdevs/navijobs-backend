32d5ac80c14cbd7c8c44fc86df7aeaeba0b006a4
noNameUser8 <ckamepova@gmail.com>
2024-09-21 14:48:30 +0500

    images

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -10,12 +10,12 @@
     <div class="container mx-auto px-4 ">
         <header class="header bg-white rounded-b-xl shadow-md flex justify-between items-center px-4 md:px-8 py-2">
             <div class="logo">
-                <img src="{{asset("images/logo 75.svg")}}" alt="" class="h-12">
+                <img src="{{asset(path:"images/logo 75.svg")}}" alt="" class="h-12">
             </div>
             <nav class="top-nav">
                 <ul class="flex gap-4">
-                    <li><a href="#" class="circle p-2 flex items-center justify-center"><img src="/img/message-question.svg" alt=""></a></li>
-                    <li><a href="#" class="circle p-2 flex items-center justify-center"><img src="/img/notification.svg" alt=""></a></li>
+                    <li><a href="#" class="circle p-2 flex items-center justify-center"><img src="{{asset("images/message-question.svg")}}" alt=""></a></li>
+                    <li><a href="#" class="circle p-2 flex items-center justify-center"><img src="{{asset(path:"images//notification.svg" )}}" alt=""></a></li>
                     <li><a href="#" class="circle p-2 flex items-center justify-center"></a></li>
                 </ul>
             </nav>
@@ -26,32 +26,32 @@
                     <ul class="flex flex-col items-center gap-4">
                         <li>
                             <button onclick="window.location.href='index.html'" class="btn-sidebar flex items-center">
-                                <img src="/img/home-2.svg" class="h-6 w-6" alt="">
+                                <img src="{{asset("images/home-2.svg")}}" alt="">
                                 <span class="ml-2">Главная</span>
                             </button>
                         </li>
                         <li>
                             <button onclick="window.location.href='requests.html'" class="btn-sidebar flex items-center">
-                                <img src="/img/directbox-notif.svg" class="h-6 w-6" alt="">
+                                <img src="{{asset("images/directbox-notif.svg")}}"  alt="">
                                 <span class="ml-2">Заявки</span>
                             </button>
                         </li>
                         <li>
                             <button onclick="window.location.href='reports.html'" class="btn-sidebar flex items-center">
-                                <img src="/img/activity.svg" class="h-6 w-6" alt="">
+                                <img   src="{{asset("images/activity.svg")}}"  alt="">
                                 <span class="ml-2">Отчеты</span>
                             </button>
                         </li>
                         <li>
                             <button onclick="window.location.href='workers.html'" class="btn-sidebar flex items-center">
-                                <img src="/img/profile-2user.svg" class="h-6 w-6" alt="">
+                                <img  src="{{asset("images/profile-2user.svg")}}"  class="h-6 w-6" alt="">
                                 <span class="ml-2">Рабочие</span>
                             </button>
                         </li>
                     </ul>
                     <div class="login-button mt-auto">
                         <button onclick="window.location.href='login.html'" class="btn-sidebar flex items-center">
-                            <img src="/img/exit.svg" class="h-6 w-6" alt="">
+                            <img  src="{{asset("images/exit.svg")}}"  class="h-6 w-6" alt="">
                             <span class="ml-2" style="color: #FF6B00;">Войти</span>
                         </button>
                     </div>
